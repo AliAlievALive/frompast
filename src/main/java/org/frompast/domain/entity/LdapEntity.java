378218d6b48c9f271dacb73a584c8cf3b1ca75fa
almir.almiev <almir.almiev@thebigdipper.com>
2025-05-05 09:45:45 +0300

    added user create method
    (cherry picked from commit 19291a22549ac108e89993938c2143c7d1713526)

--- src/main/java/org/frompast/domain/entity/LdapEntity.java
+++ src/main/java/org/frompast/domain/entity/LdapEntity.java
@@ -122,4 +122,5 @@ public class LdapEntity {
     @Attribute(name = "Enabled")
     String enabled;
 
+    String password;
 }
--- src/main/java/org/frompast/service/LdapService.java
+++ src/main/java/org/frompast/service/LdapService.java
@@ -1,24 +1,32 @@
 package org.frompast.service;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import jakarta.validation.constraints.NotNull;
 import org.frompast.configuration.properties.LdapCustomProperties;
 import org.frompast.domain.entity.LdapEntity;
 import org.frompast.domain.repository.LdapCustomRepository;
 import org.frompast.mapper.LdapEntityMapper;
 import org.springframework.ldap.control.PagedResultsCookie;
 import org.springframework.ldap.control.PagedResultsDirContextProcessor;
+import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.stereotype.Service;
 
+import javax.naming.Name;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.ModificationItem;
 import javax.naming.directory.SearchControls;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.nio.charset.StandardCharsets.UTF_16LE;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
@@ -68,4 +76,39 @@ public class LdapService {
         return "(&(objectClass=user)(|" + p + "))";
     }
 
+    @SneakyThrows
+    public void create(LdapEntity ldapEntity) {
+        log.info("Creating new LDAP user: {}", ldapEntity.getUserPrincipalName());
+
+        DirContextAdapter context = new DirContextAdapter(buildDn(ldapEntity));
+
+        context.setAttributeValues("objectClass", new String[] {"top", "person", "organizationalPerson", "user"});
+
+        ldapEntityMapper.toContext(ldapEntity, context);
+
+        ldapTemplate.bind(context);
+
+        if (ldapEntity.getPassword() != null) {
+            ldapTemplate.modifyAttributes(
+                    buildDn(ldapEntity),
+                    new ModificationItem[] {
+                            new ModificationItem(
+                                    DirContext.REPLACE_ATTRIBUTE,
+                                    new BasicAttribute("unicodePwd", encodePassword(ldapEntity.getPassword())))
+                    });
+        }
+    }
+
+    @SneakyThrows
+    private byte[] encodePassword(String password) {
+        String quotedPassword = "\"" + password + "\"";
+        return quotedPassword.getBytes(UTF_16LE);
+    }
+
+    private Name buildDn(LdapEntity ldapEntity) {
+        return LdapNameBuilder.newInstance()
+                .add("CN", ldapEntity.getCommonName())
+                .add(properties.getUserBase())
+                .build();
+    }
 }
